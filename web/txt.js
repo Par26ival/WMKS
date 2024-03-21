41998b2eeab292895b4793297b3fefbe8b93961c
cockSlayerr <164410586+cockSlayerr@users.noreply.github.com>
2024-03-21 22:59:30 +0200

    fixed delay

--- web/txt.js
+++ web/txt.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   var animationContainer = document.querySelector(".animation-container");
+  var notificationContainer = document.querySelector(".notification-container");
+
   setTimeout(function () {
     animationContainer.classList.add("hidden");
+
+    setTimeout(function () {
+      notificationContainer.style.display = "block";
+    }, 1000);
   }, 2300);
 });
