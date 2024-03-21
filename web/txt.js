b98483d213964de7decce65d60b3a80becdb8e98
cockSlayerr <164410586+cockSlayerr@users.noreply.github.com>
2024-03-21 22:28:30 +0200

    Update txt.js

--- web/txt.js
+++ web/txt.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
   var animationContainer = document.querySelector(".animation-container");
   setTimeout(function () {
     animationContainer.classList.add("hidden");
-  }, 2000);
+  }, 2300);
 });
