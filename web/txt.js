7f35914aaca1c6f22179be569d74034226d07709
cockSlayerr <164410586+cockSlayerr@users.noreply.github.com>
2024-03-21 22:27:33 +0200

    intro to site

---
+++ web/txt.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var animationContainer = document.querySelector(".animation-container");
+  setTimeout(function () {
+    animationContainer.classList.add("hidden");
+  }, 2000);
+});
