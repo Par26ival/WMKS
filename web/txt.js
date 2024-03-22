bc4d301042b10b678fd04db7848eb8f5c99cf7b8
cockSlayerr <164410586+cockSlayerr@users.noreply.github.com>
2024-03-22 19:00:56 +0200

    faide in done

--- web/txt.js
+++ web/txt.js
@@ -10,3 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1000);
   }, 2300);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButton = document.getElementById("filter-button");
+  const filterOptions = document.getElementById("filter-options");
+
+  filterButton.addEventListener("click", function () {
+    filterOptions.classList.toggle("block");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterButton = document.getElementById("filter-button");
+  const filterOptions = document.getElementById("filter-options");
+
+  filterButton.addEventListener("click", function () {
+    filterOptions.classList.toggle("opened");
+    filterOptions.classList.toggle("slide-animation");
+  });
+});
