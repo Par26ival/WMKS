beabef3dfbd3a55cdd927bd840359198175c396f
cockSlayerr <164410586+cockSlayerr@users.noreply.github.com>
2024-03-23 00:29:17 +0200

    init firebase

---
+++ web/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtvkpIbRUoF7lwBstRIfaxX5-n5iqI1PA",
+  authDomain: "tresspassing-sys.firebaseapp.com",
+  projectId: "tresspassing-sys",
+  storageBucket: "tresspassing-sys.appspot.com",
+  messagingSenderId: "1076048982269",
+  appId: "1:1076048982269:web:c690e18a2eb53d8ebc4e94"
+};
+
+const app = initializeApp(firebaseConfig);
