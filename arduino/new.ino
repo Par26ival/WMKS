c86ebaf36a3ba730e6563ca6bc45cb7cff73c774
cockSlayerr <164410586+cockSlayerr@users.noreply.github.com>
2024-03-27 10:36:04 +0200

    BACHKA WE

--- arduino/new.ino
+++ arduino/new.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+// Define the pin number for PIR sensor
+const int pirPin = 3;
 
+void setup() {
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+  pinMode(pirPin, INPUT); // Set PIR pin as input
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int pirState = digitalRead(pirPin); // Read the state of PIR sensor
+  
+  if (pirState == HIGH) { // If PIR sensor detects motion
+    Serial.println("I'm alive"); // Print "I'm alive" to serial monitor
+    Serial.println("kaloqn e gay");
+    delay(1000); // Delay to avoid printing continuously for a single motion
+  }
 }
+     
