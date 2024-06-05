4b4a01b01a36596fba534901d96190e31671d940
Walk3r1 <105781742+Walk3r1@users.noreply.github.com>
2024-06-05 08:44:41 +0300

    opraweni animationi i notifs

--- web/public/txt.js
+++ web/public/txt.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   var animationContainer = document.querySelector(".animation-container");
   var notificationContainer = document.querySelector(".notification-container");
+  var notifications = document.querySelector(".notifications");
 
   setTimeout(function () {
     animationContainer.classList.add("hidden");
 
     setTimeout(function () {
       notificationContainer.style.display = "block";
+      notifications.classList.remove("hidden");
+      notifications.classList.add("visible");
     }, 1000);
   }, 2300);
 });
